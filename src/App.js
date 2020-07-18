2e4d8f95195d60321377cbd4ea36a3b77beee091
Konrad Bil <konrad_bil@t-online.de>
2020-07-18 17:33:35 +0530

    add Shallow Rendering with Enzyme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import EventList from './EventList';
+import Event from './Event';
+
+//style
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<CitySearch />
+				<NumberOfEvents />
+				<EventList />
+				<Event />
+			</div>
+		);
+	}
 }
 
 export default App;
